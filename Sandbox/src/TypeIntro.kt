c4c72921390736fdbbb6c37170839f93854d654c
Adam Johns <robert.adam.johns@gmail.com>
2019-05-09 20:19:57 -0500

    Chapter 2 complete

---
+++ Sandbox/src/TypeIntro.kt
@@ -0,0 +1,15 @@
+const val MAX_EXPERIENCE = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    val playerNameReversed = playerName.reversed()
+    var hasSteed = false
+    val pubName = "Pub"
+    var publicanOnDuty = "Steve"
+    var goldPieces = 50
+    val drinks = setOf("mead", "wine", "lacroix")
+    var experiencePoints = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerNameReversed)
+}
